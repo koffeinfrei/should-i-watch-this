6eea0654cbc9e111019b30f019db8cb5e3d9f639
Alexis Reigel <mail@koffeinfrei.org>
2020-12-18 16:13:15 +0100

    add meta tags for social media (-> preview)

--- src/components/Result.svelte
+++ src/components/Result.svelte
@@ -1,7 +1,18 @@
 <script>
   import ResultMoreLink from '../components/ResultMoreLink.svelte';
+  import { stores } from '@sapper/app';
 
   export let movie;
+
+  const { page } = stores();
+  const { host, path } = $page;
+  const protocol = host.startsWith('localhost') ? 'http://' : 'https://';
+  const hostWithProtocol = `${protocol}${host}`
+  const pageUrl = `${hostWithProtocol}${path}`
+
+  const posterUrl = movie.poster_url || `${hostWithProtocol}/default-poster.png`;
+
+  const pageTitle = `${movie.title} (${movie.year}) | Should I watch this?`;
 </script>
 
 <style type="text/scss">
@@ -114,7 +125,7 @@ h3 {
     </div>
 
     <div class="marketing">
-      <img src={movie.poster_url || '/default-poster.png'} width="130" class="poster" alt="poster" />
+      <img src={posterUrl} width="130" class="poster" alt="poster" />
       {#if movie.trailer_url}
         <a href={movie.trailer_url} target="_blank" class="external trailer">Trailer</a>
       {/if}
@@ -164,5 +175,19 @@ h3 {
 </section>
 
 <svelte:head>
-  <title>{movie.title} ({movie.year}) | Should I watch this?</title>
+  <title>{pageTitle}</title>
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website">
+  <meta property="og:url" content={pageUrl}>
+  <meta property="og:title" content={pageTitle}>
+  <meta property="og:description" content="{movie.recommendation.emoji} {movie.recommendation.text}">
+  <meta property="og:image" content={posterUrl}>
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content={pageUrl}>
+  <meta property="twitter:title" content={pageTitle}>
+  <meta property="twitter:description" content="{movie.recommendation.emoji} {movie.recommendation.text}">
+  <meta property="twitter:image" content={posterUrl}>
 </svelte:head>
