e1e553db580253a6753c65499e4e25559375a361
Alexis Reigel <mail@koffeinfrei.org>
2020-12-18 14:03:01 +0100

    fix autofocus
    sapper actually resets the focus in favor of ally:
    https://github.com/sveltejs/sapper/issues/619

--- src/components/SearchBox.svelte
+++ src/components/SearchBox.svelte
@@ -1,14 +1,8 @@
 <script>
-  import { onMount } from 'svelte';
-
   export let title = undefined;
   export let onSubmit;
 
   let titleInput;
-
-  onMount(async () => {
-    titleInput.focus();
-  });
 
   async function handleClick() {
     onSubmit(title);
@@ -20,9 +14,12 @@
     }
   }
 
+  async function init(element){
+    setTimeout(() => element.focus(), 0);
+  }
 </script>
 
 <section class="box padded">
-  <input bind:this={titleInput} bind:value={title} on:keyup={handleKeyup} placeholder="Movie title or IMDb id">
+  <input bind:this={titleInput} bind:value={title} on:keyup={handleKeyup} placeholder="Movie title or IMDb id" use:init>
   <button on:click={handleClick}>Look up</button>
 </section>
