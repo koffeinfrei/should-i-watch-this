b02a55d788172ffc2375dd5c6df4d1baa75f1660
Alexis Reigel <mail@koffeinfrei.org>
2020-12-21 21:22:59 +0100

    trigger prefetch automatically
    the programmatic call to `prefetch` doesn't seem to work. triggering the
    `rel=prefetch` on the link elements seems to do the trick.

--- src/components/RandomQuote.svelte
+++ src/components/RandomQuote.svelte
@@ -1,11 +1,19 @@
 <script>
   import { getRandomQuote } from '../quotes';
+  import { onMount } from 'svelte';
 
+  let movieLink;
   // we render the quote only client side, otherwise we end up with 2 different
   // random quotes
   let quote;
   if (typeof window !== 'undefined') {
     quote = getRandomQuote();
+
+    onMount(() => {
+      // the programmatic call to `prefetch` doesn't seem to work. triggering
+      // the `rel=prefetch` on the link elements seems to do the trick.
+      movieLink.dispatchEvent(new MouseEvent('touchstart', { 'bubbles': true }));
+    });
   }
 </script>
 
@@ -17,7 +25,7 @@
 <div class="box">
   <div class="icon-label"><span class="icon">💭</span>
     <div>“ {quote.quote} ”</div>
-    <a rel=prefetch href={quote.url}>{quote.movie}</a>
+    <a bind:this={movieLink} rel=prefetch href={quote.url}>{quote.movie}</a>
   </div>
 </div>
 {/if}
