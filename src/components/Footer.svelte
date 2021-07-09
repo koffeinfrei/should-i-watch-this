a75d39a3b64fb79063d8e243c322b616318d696c
Alexis Reigel <mail@koffeinfrei.org>
2021-07-09 17:18:50 +0200

    github link to merged repo instead of *-www

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -21,6 +21,6 @@ p {
     Made with ☕️  by <a href="https://www.koffeinfrei.org" class="external" target="_blank" rel="noopener external">Koffeinfrei</a>
     <br>
     This is free and open source software.
-    Source code at <a href="https://github.com/koffeinfrei/should-i-watch-this-www" class="external" target="_blank" rel="noopener external">GitHub</a>
+    Source code at <a href="https://github.com/koffeinfrei/should-i-watch-this" class="external" target="_blank" rel="noopener external">GitHub</a>
   </p>
 </section>
