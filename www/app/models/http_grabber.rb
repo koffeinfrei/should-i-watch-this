ff830832741952a769641a4f95803414f8764977
Alexis Reigel <mail@koffeinfrei.org>
2025-10-04 10:14:06 +0200

    replace mechanize by curl / nokogiri
    The requests to rotten tomatoes started erroring out with 403 all of a
    sudden. I tried tweaking different headers, but I couldn't make it work.
    Curl doesn't seem to have that problem.
    
    Didnn't work:
    
    ```
    uri = URI.parse("https://www.rottentomatoes.com/m/her")
    req = Net::HTTP::Get.new(uri)
    # These are the headers curl sends
    req['User-Agent'] = 'curl/8.5.0'
    req['Accept'] = '*/*'
    req['Host'] = 'www.rottentomatoes.com'
    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) {|http|
      http.request(req)
    }
    res.body
    ```
    
    Works:
    
    ```
    curl 'https://www.rottentomatoes.com/m/her'
    ```
    
    Curl seems to do something differently about requests that ruby doesn't.
    I couldn't figure out what. Since we don't really need the feature set
    of mechanize I replaced it with a curl binding gem and a simple nokogiri
    parse.
    
    Mechanize is currently still used for the rake task. I may replace it in
    the future.

--- www/app/models/http_grabber.rb
+++ www/app/models/http_grabber.rb
@@ -4,14 +4,14 @@ class HttpGrabber
   end
 
   def run(selector)
-    agent = Mechanize.new
-    agent.user_agent_alias = "Mac Safari"
-    agent.read_timeout = 5
-    agent.open_timeout = 5
-
-    page = agent.get(@url)
-    page.search(selector).first.content
-  rescue Mechanize::ResponseCodeError, Net::ReadTimeout, Net::OpenTimeout => e
+    response = Curl.get(@url) do |http|
+      http.connect_timeout = 5
+      http.timeout = 5
+      http.follow_location = true
+    end
+    page = Nokogiri::HTML(response.body_str)
+    page.at_css(selector)&.content
+  rescue StandardError => e
     Rails.logger.error("event=http_grabber_error error=#{e.inspect}")
     nil
   end
