10311f1767fed9f43ec6c0a524ff95c848d3cb30
Alexis Reigel <mail@koffeinfrei.org>
2025-11-14 19:29:00 +0100

    fix imdb scraping
    IMDb started rejecting the vanilla curl request, i.e. responding with:
    
    ```
    HTTP/2 202
    server: CloudFront
    date: Fri, 14 Nov 2025 18:29:46 GMT
    content-length: 0
    x-amzn-waf-action: challenge
    cache-control: no-store, max-age=0
    content-type: text/html; charset=UTF-8
    access-control-allow-origin: *
    access-control-max-age: 86400
    access-control-allow-methods: OPTIONS,GET,POST
    access-control-expose-headers: x-amzn-waf-action
    x-cache: Error from cloudfront
    via: 1.1 f106bccd16721a0e4902884b35263d12.cloudfront.net (CloudFront)
    x-amz-cf-pop: ZRH52-P1
    alt-svc: h3=":443"; ma=86400
    x-amz-cf-id: _wmwiojqm70G_L1sEcatCvhvYXzmfXOg3bTxiUHnnAKJABsodg8Azw==
    ```
    
    That resulted in an empty body, which failed the `ScoreFetcher`.
    The `Accept` header alone would result in a bot JS challenge:
    
    ```
    <noscript>
        <h1>JavaScript is disabled</h1>
        In order to continue, we need to verify that you're not a robot.
        This requires JavaScript. Enable JavaScript and then reload the page.
    </noscript>
    ```
    
    Adding the `User-Agent` header seems to do the trick. For now.

--- www/app/models/http_grabber.rb
+++ www/app/models/http_grabber.rb
@@ -8,6 +8,8 @@ class HttpGrabber
       http.connect_timeout = 5
       http.timeout = 5
       http.follow_location = true
+      http.headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+      http.headers["User-Agent"] = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:145.0) Gecko/20100101 Firefox/145.0"
     end
     page = Nokogiri::HTML(response.body_str)
     page.at_css(selector)&.content
