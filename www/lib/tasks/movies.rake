koffeinfrei/should-i-watch-this

commit 7bab78fc9a05ad1c966fc70e5f2af7c0858e65c1

make all posters equal width
Amends 5132179 and makes sure that all images have the same width.
The default poster would sometimes not show the full border because of
the `object-fit`. We now add the border in CSS and removed it from the
png.

www/app/frontend/entrypoints/application.scss
@@ -401,10 +401,11 @@ a.result-more {
   }
 
   &-poster {
-    max-width: 65px;
+    width: 60px;
     height: 90px;
     margin-right: 0.6em;
     object-fit: cover;
+    border: 1px solid #000;
   }
 
   &-title {

commit 514f283a02fc516334434f6e8dd0ccca194cdf64

add border to movie page poster
This regressed with 7bab78f.

www/app/frontend/entrypoints/application.scss
@@ -29,6 +29,7 @@ $spacing-horizontal: 50px;
 
 $left-column-width: 9rem;
 $poster-width: 130px;
+$poster-border: 1px solid #000;
 
 $breakpoint-sm: 40em;
 $breakpoint-md: 800px;
@@ -364,6 +365,7 @@ h2.movie {
 .poster {
   width: $poster-width;
   height: auto;
+  border: $poster-border;
 }
 
 .marketing {
@@ -418,7 +420,7 @@ a.result-more {
     height: 90px;
     margin-right: 0.6em;
     object-fit: cover;
-    border: 1px solid #000;
+    border: $poster-border;
   }
 
   &-title {

www/lib/tasks/movies.rake
@@ -174,15 +174,15 @@ namespace :movies do
       agent.read_timeout = 5
       agent.open_timeout = 5
 
-      out_dir = Rails.root.join("public/posters")
+      out_dir = Rails.root.join("public/posters/original")
       out_dir_100 = Rails.root.join("public/posters/100")
       out_dir_300 = Rails.root.join("public/posters/300")
       log_none = Rails.root.join("tmp/fetch_posters_imdb_none")
       log_errors = Rails.root.join("tmp/fetch_posters_imdb_errors")
 
       FileUtils.touch(log_none) unless File.exist?(log_none)
-      FileUtils.mkdir_p(out_dir_300)
       FileUtils.mkdir_p(out_dir_100)
+      FileUtils.mkdir_p(out_dir_300)
 
       no_poster_input = File.readlines(log_none).map(&:strip)
 
@@ -224,8 +224,8 @@ namespace :movies do
           `convert #{tmp_path} #{target_path}`
           FileUtils.rm tmp_path
         end
-        `convert -resize 100x #{target_path} #{out_dir.join("100/#{wiki_id}.jpg")}`
-        `convert -resize 300x #{target_path} #{out_dir.join("300/#{wiki_id}.jpg")}`
+        `convert -resize 100x #{target_path} #{out_dir_100.join("#{wiki_id}.jpg")}`
+        `convert -resize 300x #{target_path} #{out_dir_300.join("#{wiki_id}.jpg")}`
 
         print "."
       rescue Mechanize::ResponseCodeError, Net::ReadTimeout, Net::OpenTimeout => error